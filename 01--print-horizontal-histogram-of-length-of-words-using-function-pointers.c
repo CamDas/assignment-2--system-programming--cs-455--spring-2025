aa5a1e384beb32c13d3d5f3e9daa842567ab64c3
KScrubbles <102969513+KScrubbles@users.noreply.github.com>
2025-03-29 17:06:56 -0400

    This does not work
    Hold onto for later

--- 01--print-horizontal-histogram-of-length-of-words-using-function-pointers.c
+++ 01--print-horizontal-histogram-of-length-of-words-using-function-pointers.c
@@ -1,1 +1,25 @@
-int main(){}
+#include <stdio.h>
+
+int user;
+int *pUser = &user;
+
+int main(){
+   printf("Choose whether to sort the array in ascending(1) or descending(2) order: \n");    // Ask the user to type a word
+
+   scanf("%d", &user);
+   *pUser = user;
+   bubbleSort();
+}
+int ascending(int *pUser){
+   if (*pUser == 1){
+      printf("balls");
+      return 0;
+   }
+}
+
+int descending(int *pUser){
+   if (*pUser != 1){
+      printf("cool!");
+      return 0;
+   }
+}
