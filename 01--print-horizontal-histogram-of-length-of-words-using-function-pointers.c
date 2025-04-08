1e3f5dd5c7af95c496251391d7ee3dfa7707ff9b
KScrubbles <102969513+KScrubbles@users.noreply.github.com>
2025-04-08 12:42:25 -0400

    Final changes

--- 01--print-horizontal-histogram-of-length-of-words-using-function-pointers.c
+++ 01--print-horizontal-histogram-of-length-of-words-using-function-pointers.c
@@ -1,25 +1,61 @@
 #include <stdio.h>
 
-int user;
-int *pUser = &user;
+// Function prototypes
+void bubbleSort(int a[], int size, int (*compare)(int, int));
+void swap(int *a, int *b);
+int ascending(int a, int b);
+int descending(int a, int b);
 
-int main(){
-   printf("Choose whether to sort the array in ascending(1) or descending(2) order: \n");    // Ask the user to type a word
+int main() {
+	int a[] = {2, 6, 4, 8, 10, 12, 89, 68, 45, 37};
+	int size = 10;
+	int choice;
 
-   scanf("%d", &user);
-   *pUser = user;
-   bubbleSort();
+	printf("Chooses whether to sort the array in ascending(1) or descending (2) order\n");
+	scanf("%d", &choice);
+
+	if (choice == 1) {
+		bubbleSort(a, size, ascending);
+	} else {
+		bubbleSort(a, size, descending);
+
+	}
+
+
+	printf("Default array: 2, 6, 4, 8, 10, 12, 89, 68, 45, 37\n");   // display the original and sorted array
+	printf("Sorted array: ");
+	for (int i = 0; i < (size); i++) {
+		if (i != 9) {
+			printf("%d, ", a[i]);   //used for all numbers in the array except for the last
+		}
+		else {
+			printf("%d", a[i]);   //prints without comma for last output
+		}
+	}
+
+	return 0;
 }
-int ascending(int *pUser){
-   if (*pUser == 1){
-      printf("balls");
-      return 0;
-   }
+
+void bubbleSort(int a[], int size, int (*compare)(int, int)) {
+	for (int i = 0; i < size - 1; i++) {
+		for (int j = 0; j < size - i - 1; j++) {
+			if (compare(a[j], a[j + 1])) {
+				swap(&a[j], &a[j + 1]);
+			}
+		}
+	}
 }
 
-int descending(int *pUser){
-   if (*pUser != 1){
-      printf("cool!");
-      return 0;
-   }
+void swap(int *a, int *b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int ascending(int a, int b) {
+	return a > b;  
+}
+
+int descending(int a, int b) {
+	return a < b;  
 }
--- 02--arrays-of-function-pointers.c
+++ 02--arrays-of-function-pointers.c
@@ -1,1 +1,91 @@
-int main(){}
+#include <stdio.h>
+#include <stdlib.h>
+
+#define STUDENTS 3
+#define EXAMS 4
+int i, j;
+int result;
+int choice;
+float avg;
+int studentGrades[STUDENTS][EXAMS] = {
+   {77, 68, 86, 73},
+   {96, 87, 89, 78},
+   {70, 90, 86, 81}};
+
+int main() {
+
+	printf("\nEnter a choice:\n 0  Print the array of grades\n 1  Find the minimum grade\n 2  Find the maximum grade\n 3  Print the average on all tests for each student\n 4  End program\n");
+	scanf("%d", &choice);
+
+	if (choice == 0) {
+		printArray();}
+   else if (choice == 1) {
+      minimum();}
+   else if (choice == 2) {
+      maximum();}
+   else if (choice == 3) {
+      average();}
+   else if (choice == 4) {
+      exit(0);}
+	}
+
+   int printArray(){
+      for (int i = 0; i < 3; i++) {
+         printf("\n");
+         for (int j = 0; j < 4; j++) {
+           printf("%d ", studentGrades[i][j]);; {
+      }
+   }
+}
+   return main();
+   }
+
+   int minimum(){
+      result = studentGrades[0][0];
+      for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 4; j++){
+            if (result > studentGrades[i][j]){
+                result = studentGrades[i][j];
+
+            }
+         }
+      }
+      printf("%d", result);
+      return main();
+   }
+
+   int maximum(){
+      result = studentGrades[0][0];
+      for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 4; j++){
+            if (result < studentGrades[i][j]){
+                result = studentGrades[i][j];
+
+            }
+         }
+      }
+      printf("%d", result);
+      return main();
+   }
+
+   int average(){
+      for (i = 0; i < 3; i++)
+    {    avg = 0;
+        for (j = 0; j < 4; j++){
+            avg = avg + studentGrades[i][j];
+
+            }
+         avg = avg / 4;
+         printf("%f", avg);
+         printf("\n");
+         }
+
+
+      return main();
+   }
+
+      void (*processGrades[4])(int [][EXAMS], size_t, size_t) =
+      {printArray, minimum, maximum, average};
+
